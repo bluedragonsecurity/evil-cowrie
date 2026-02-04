7aa9acc91c25fae2bfe73d2eb4c96e8632188496
BlueDragonSecurity <144339177+bluedragonsecurity@users.noreply.github.com>
2026-02-04 19:25:10 +0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo iptables -t nat -F
-sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j DNAT --to-destination 0.0.0.0:2222
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j DNAT --to-destination 0.0.0.0:2222
 sudo iptables -t nat -A OUTPUT -p tcp --dport 22 -m owner --uid-owner root -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 sudo iptables -t nat -A OUTPUT -p tcp --dport 22 -m owner --uid-owner root -j ACCEPT
